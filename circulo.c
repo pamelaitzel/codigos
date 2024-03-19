eb7f80905c9f560c2d12d20221511bd4c23f28ac
pamelaitzel <135767172+pamelaitzel@users.noreply.github.com>
2024-03-18 21:09:43 -0600

    Create circulo.c

---
+++ circulo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define PI 3.14159265
+
+int main() {
+    double radio = 5.0;
+    double area = PI * radio * radio;
+    printf("El área del círculo es: %f\n", area);
+    return 0;
+}
